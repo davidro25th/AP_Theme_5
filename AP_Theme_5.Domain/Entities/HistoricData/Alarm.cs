a2193e7dbd8873bb64e133b9895a920bb5fc130d
Frank Leyva <frankkleyva@gmail.com>
2024-06-04 22:11:04 -0700

    Adding Alarm class and Priority Enum

---
+++ AP_Theme_5.Domain/Entities/HistoricData/Alarm.cs
@@ -0,0 +1,19 @@
+﻿using AP_Theme_5.Domain.Entities.Configuration_Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AP_Theme_5.Domain.Entities.HistoricData
+{
+    public class Alarm
+    {
+        public DateTime Incidencedate { get; set; }
+        public DateTime Recoverydate { get; set; }
+        public Priority Priority { get; set; }
+        public double OutOfRange { get; set; }
+        public Variable AlarmVariable { get; set; }
+
+    }
+}
---
+++ AP_Theme_5.Domain/Entities/HistoricData/Priority.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AP_Theme_5.Domain.Entities.HistoricData
+{
+    public enum Priority
+    {
+        Low, Medium, High
+    }
+}
