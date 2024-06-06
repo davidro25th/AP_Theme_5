fb3c89005def4e4be9ef6b873d45dd3e318a6f55
Carly011014 <carlosmanuelmarquez92@gmail.com>
2024-06-05 21:32:19 -0400

    Adding class Audit_Event Changing HistoricData to Historical Data

---
+++ AP_Theme_5.Domain/Entities/Historical Data/Audit_Event.cs
@@ -0,0 +1,27 @@
+﻿using AP_Theme_5.Domain.Entities.Configuration_Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AP_Theme_5.Domain.Entities.HistoricData
+{
+    public class Audit_Event
+    {
+        protected string action;
+        protected DateTime ocurrence;
+        protected Worker worker;
+
+        public string Action {  get { return action; } }
+        public DateTime Ocurrence { get {  return ocurrence; } }
+        public Worker Worker { get { return worker; } }
+
+        public Audit_Event(string action, DateTime ocurrence, Worker worker)
+        {
+            this.action = action;
+            this.ocurrence = ocurrence;
+            this.worker = worker;
+        }
+    }
+}
