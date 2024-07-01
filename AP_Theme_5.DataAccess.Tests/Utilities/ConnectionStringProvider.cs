2020012b157a3793a6d7ecf75414a6f00164a138
David <David@DESKTOP-OEL89OB>
2024-06-30 20:40:08 -0400

    Creating New Class Library for Unit Test Functionality (WIP)

---
+++ AP_Theme_5.DataAccess.Tests/Utilities/ConnectionStringProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AP_Theme_5.DataAccess.Tests.Utilities
+{
+    /// <summary>
+    /// Proveedor de cadena de Conexion
+    /// </summary>
+    public static class ConnectionStringProvider
+    {
+        public static string GetConnectionString() => "Data.Source=Data.sqlite";
+    }
+}
---
+++ AP_Theme_5.DataAccess.Tests/VariableTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AP_Theme_5.DataAccess.Tests
+{
+    [TestClass]
+    internal class VariableTest
+    {
+
+    }
+}
