3447b005f01fff370ab633ea839a779cd1891f4c
Frank Leyva <frankkleyva@gmail.com>
2024-09-04 23:12:04 -0700

    some fixes left behind

--- GrpcService1/Mappers/AuditEventProfile.cs
+++ GrpcService1/Mappers/AuditEventProfile.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Google.Protobuf.WellKnownTypes;
 
 
 namespace GrpcService1.Mappers
@@ -7,17 +8,22 @@ namespace GrpcService1.Mappers
     {
         public AuditEventProfile()
         {
+            CreateMap<DateTime, Timestamp>()
+            .ConvertUsing(src => Timestamp.FromDateTime(src.ToUniversalTime()));
             CreateMap<AP_Theme_5.Domain.Entities.HistoricData.AuditEvent, AP_Theme_5.GrpcProtos.AuditEventDTO>()
                 .ForMember(t => t.Id, o => o.MapFrom(s => s.Id.ToString()))
-                .ForMember(t => t.Action, o => o.MapFrom(t => t.Action))
-                .ForMember(t => t.Ocurrence, o => o.MapFrom(t => t.Ocurrence))
-                .ForMember(t => t.Worker, o => o.MapFrom(t => t.Worker));
+                .ForMember(t => t.Action, o => o.MapFrom(s => s.Action))
+                .ForMember(t => t.Ocurrence, o => o.MapFrom(s => s.Ocurrence))
+                .ForMember(t => t.Worker, o => o.MapFrom(s => s.Worker));
+
+            CreateMap<Timestamp, DateTime>()
+            .ConvertUsing(src => src.ToDateTime());
 
             CreateMap<AP_Theme_5.GrpcProtos.AuditEventDTO, AP_Theme_5.Domain.Entities.HistoricData.AuditEvent>()
                 .ForMember(t => t.Id, o => o.MapFrom(s => new Guid(s.Id)))
-                .ForMember(t => t.Action, o => o.MapFrom(t => t.Action))
-                .ForMember(t => t.Ocurrence, o => o.MapFrom(t => t.Ocurrence))
-                .ForMember(t => t.Worker, o => o.MapFrom(t => t.Worker));
+                .ForMember(t => t.Action, o => o.MapFrom(s => s.Action))
+                .ForMember(t => t.Ocurrence, o => o.MapFrom(s => s.Ocurrence))
+                .ForMember(t => t.Worker, o => o.MapFrom(s => s.Worker));
 
         }
     }
